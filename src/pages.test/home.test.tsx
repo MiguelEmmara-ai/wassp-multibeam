58e4b69ab0dadb1c4c1c738074200c5fca0055ca
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-20 18:19:32 +1200

    Add MUI dependencies and Button component

--- src/pages.test/home.test.tsx
+++ src/pages.test/home.test.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import Home from '../pages/index'
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from '../pages/index';
 
 describe('Home', () => {
-  it('should render the Home Page text', () => {
-    const { getByText } = render(<Home />)
+  it('should render the MUI button', () => {
+    const { getByRole } = render(<Home />);
 
-    expect(getByText('Home Page')).toBeInTheDocument()
-  })
-})
+    const button = getByRole('button');
+
+    expect(button).toBeInTheDocument();
+  });
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
+import ButtonUsage from '@/components/Button'
+
 const Home = () => {
   return (
-      <div>Home Page</div>
+    <section className="flex">
+      <div className="container">
+        <h1>Test MUI Button</h1>
+        <ButtonUsage />
+      </div>
+    </section>
   )
 }
 
