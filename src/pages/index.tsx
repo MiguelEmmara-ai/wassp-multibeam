eadf7f4d1d83e981acbb5d614fa9abaae667e707
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-20 19:24:52 +1200

    Update dependencies, refactor button component, add theme support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,13 @@ const Home = () => {
   return (
     <section className="flex">
       <div className="container">
+        <h1>Test Normal Button</h1>
+        <button
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'
+        >CLICK ME!</button>
+      </div>
+
+      <div className="container">
         <h1>Test MUI Button</h1>
         <ButtonUsage />
       </div>
---
+++ src/styles/theme.js
@@ -0,0 +1,7 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  /* Add your custom MUI theme overrides here */
+});
+
+export default theme;
